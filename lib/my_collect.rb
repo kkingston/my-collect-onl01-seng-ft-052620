b9506eb88b5c08d050e825127c1090251319a1ea
Kayla Kingston <kkingston09@gmail.com>
2020-06-02 00:40:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,11 +4,11 @@ def my_collect(array)
   new_array = []
   index = 0 
 
-    while index < array.length 
-      new_array << yield (array[index])
-      index += 1 
-    end
-    new_array
+  while index < array.length 
+    new_array << yield (array[index])
+    index += 1 
+  end
+  new_array
 end 
 
 # ["Tim Jones", "Tom Smith", "Jim Campagno"]
